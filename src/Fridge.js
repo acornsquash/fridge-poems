7fc4e8bfe06edf6943c44cf8c75724831f426f73
inakins <53452799+inakins@users.noreply.github.com>
2021-08-06 19:06:12 -0400

    buttons change words based on state

--- src/Fridge.js
+++ src/Fridge.js
@@ -18,17 +18,59 @@ export default class Fridge extends React.Component {
         this.handleClick = this.handleClick.bind(this)
     }
 
-    handleClick() {
-        this.setState({
-            one: RandomWord()
-        })
+    handleClick(id) { 
+        switch(id) {
+            case 'one':
+            this.setState({
+                one: RandomWord()
+            })
+            break
+            case 'two':
+            this.setState({
+                two: RandomWord()
+            })
+            break
+            case 'three':
+            this.setState({
+                three: RandomWord()
+            })
+            break
+            case 'four':
+            this.setState({
+                four: RandomWord()
+            })
+            break
+            case 'five':
+            this.setState({
+                five: RandomWord()
+            })
+            break
+            case 'six':
+            this.setState({
+                six: RandomWord()
+            })
+            break
+            case 'seven':
+            this.setState({
+                seven: RandomWord()
+            })
+            break
+            default: 
+            this.setState = intitialState
+        }
+        
     }
+
     render() {
         return(
             <div>
-            <button onClick={() => this.handleClick()}>{this.state.one}</button>
-            <button></button>
-            <button> </button>
+            <button onClick={() => this.handleClick('one')}>{this.state.one}</button>
+            <button onClick={() => this.handleClick('two')}>{this.state.two}</button>
+            <button onClick={() => this.handleClick('three')}>{this.state.three}</button>
+            <button onClick={() => this.handleClick('four')}>{this.state.four}</button>
+            <button onClick={() => this.handleClick('five')}>{this.state.five}</button>
+            <button onClick={() => this.handleClick('six')}>{this.state.six}</button>
+            <button onClick={() => this.handleClick('seven')}>{this.state.seven}</button>
             </div>
         )
     }
