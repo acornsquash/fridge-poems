483b0bd9a612ce3b5a668bf2b068059abdeae786
inakins <53452799+inakins@users.noreply.github.com>
2021-08-06 18:41:59 -0400

    buttons show words

--- src/Fridge.js
+++ src/Fridge.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import RandomWord from 'random-words'
 
-
+const intitialState = {
+    one: '',
+    two: '',
+    three: '',
+    four: '',
+    five: '',
+    six: '',
+    seven: ''
+}
 
 export default class Fridge extends React.Component {
     constructor() {
         super()
+        this.state = intitialState
         this.handleClick = this.handleClick.bind(this)
     }
 
     handleClick() {
-        console.log(RandomWord())
+        this.setState({
+            one: RandomWord()
+        })
     }
     render() {
         return(
-            <button onClick={() => this.handleClick()}></button>
+            <div>
+            <button onClick={() => this.handleClick()}>{this.state.one}</button>
+            <button></button>
+            <button> </button>
+            </div>
         )
     }
 }
